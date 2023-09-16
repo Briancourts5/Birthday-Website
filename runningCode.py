36cf7e39818855eba894c7bda0bec64482a62208
Briancourts5 <bcourts55555@gmail.com>
2023-09-16 17:34:03 -0400

    Allowed different number of table entries

--- runningCode.py
+++ runningCode.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import mysql.connector
 from datetime import date
+import mysql.connector
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
@@ -10,7 +11,7 @@ def loadInformation():
     startDate = date(2023, 6, 29)
     currentDate = date.today()
     position = (currentDate - startDate).days + 1
-    position = position % 100
+    # position = position % 100
 
     mydb = mysql.connector.connect(
         host="localhost",
@@ -21,27 +22,45 @@ def loadInformation():
 
     cursor = mydb.cursor()
 
-    cursor.execute("SELECT fact FROM facts WHERE id = " + str(position))
+    cursor.execute("SELECT COUNT(ID) FROM facts")
+    numberOfElephantFacts = int(cursor.fetchone()[0])
+    
+    cursor.execute("SELECT COUNT(ID) FROM jokes")
+    numberOfJokes = int(cursor.fetchone()[0])
+
+    cursor.execute("SELECT COUNT(ID) FROM pickupLines")
+    numberOfPickupLines = int(cursor.fetchone()[0])
+
+    cursor.execute("SELECT COUNT(ID) FROM science")
+    numberOfScienceFacts = int(cursor.fetchone()[0])
+
+    cursor.execute("SELECT COUNT(ID) FROM general")
+    numberOfGeneralFacts = int(cursor.fetchone()[0])
+
+    cursor.execute("SELECT COUNT(ID) FROM love")
+    numberOfLoveTranslations = int(cursor.fetchone()[0])
+
+    cursor.execute("SELECT fact FROM facts WHERE id = " + str(position % numberOfElephantFacts))
     elephantFact = cursor.fetchone()
     elephantFact = ["Elephant Fact: ", elephantFact[0]]
 
-    cursor.execute("SELECT joke FROM jokes WHERE id = " + str(position))
+    cursor.execute("SELECT joke FROM jokes WHERE id = " + str(position % numberOfJokes))
     badJoke = cursor.fetchone()
     badJoke = ["Joke: ", badJoke[0]]
 
-    cursor.execute("SELECT line FROM pickupLines WHERE id = " + str(position))
+    cursor.execute("SELECT line FROM pickupLines WHERE id = " + str(position % numberOfPickupLines))
     pickupLine = cursor.fetchone()
     pickupLine = ["Pickup Line: ", pickupLine[0]]
 
-    cursor.execute("SELECT fact FROM science WHERE id = " + str(position))
+    cursor.execute("SELECT fact FROM science WHERE id = " + str(position % numberOfScienceFacts))
     scienceFact = cursor.fetchone()
     scienceFact = ["Science Fact: ", scienceFact[0]]
 
-    cursor.execute("SELECT fact FROM general WHERE id = " + str(176))
+    cursor.execute("SELECT fact FROM general WHERE id = " + str(position % numberOfGeneralFacts))
     generalFact = cursor.fetchone()
     generalFact = ["General Fact: ", generalFact[0]]
 
-    cursor.execute("SELECT translation FROM love WHERE id = " + str(position))
+    cursor.execute("SELECT translation FROM love WHERE id = " + str(position % numberOfLoveTranslations))
     loveTranslation = cursor.fetchone()
     loveTranslation = ["I Love You: ", loveTranslation[0]]
 
